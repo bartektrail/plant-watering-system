e552393f27202cc1fa06920e28a0ad4dce14d539
Bartek Kwiatkowski <bartektrail@users.noreply.github.com>
2018-01-05 15:30:25 +0100

    Sensor VCC is now controlled by digital pin 9

--- soil_moisture_sensor.ino
+++ soil_moisture_sensor.ino
@@ -1,58 +1,72 @@
 #define A0  A0
-int led12 = 12;
-int led11 = 11;
-int led10 = 10;
+int led12green = 12;
+int led11yellow = 11;
+int led10red = 10;
+int sensorPower9 = 9;
 
 int analogValue = 0;
 int percentageValue = 0;
 
+int MAX_SENSOR_VALUE = 600; // measured by putting moisture sensor into water
+int MIN_SENSOR_VALUE = 6; // measured  by putting moisture sensor in the air (not used)
+int SENSOR_INIT_TIME = 3*1000; // we have to wait for stabilisation after power is turned on
+int SLEEP_TIME = 4*60*1000; // sleeps to increase life time of sensor. For develepment change to 5*1000
+
+/**
+ * @author Bartek Kwiatkowski, 2018
+ */
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(A0, INPUT);
-  pinMode(led12, OUTPUT);
-  pinMode(led11, OUTPUT);
-  pinMode(led10, OUTPUT);
+  pinMode(led12green, OUTPUT);
+  pinMode(led11yellow, OUTPUT);
+  pinMode(led10red, OUTPUT);
+  pinMode(sensorPower9, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  analogValue = analogRead(A0);
+  digitalWrite(sensorPower9, HIGH);
+  delay(SENSOR_INIT_TIME);
   
-//  Serial.println( 1023 - analogRead(A0));
-
+  analogValue = analogRead(A0);
+  percentageValue = moisture(analogValue);
+  
+  Serial.println(analogValue);
+  Serial.println(percentageValue);
   Serial.println(moistureToString(analogValue));
   
-  percentageValue = moisture(analogValue);
 
   if (percentageValue >= 60) {
-    digitalWrite(led12, HIGH);
-    digitalWrite(led11, LOW);
-    digitalWrite(led10, LOW);
+    digitalWrite(led12green, HIGH);
+    digitalWrite(led11yellow, LOW);
+    digitalWrite(led10red, LOW);
   } else if (percentageValue >= 40) {
-    digitalWrite(led12, LOW);
-    digitalWrite(led11, HIGH);
-    digitalWrite(led10, LOW);
+    digitalWrite(led12green, LOW);
+    digitalWrite(led11yellow, HIGH);
+    digitalWrite(led10red, LOW);
   } else {
-    digitalWrite(led12, LOW);
-    digitalWrite(led11, LOW);
-    digitalWrite(led10, HIGH);
+    digitalWrite(led12green, LOW);
+    digitalWrite(led11yellow, LOW);
+    digitalWrite(led10red, HIGH);
   }
   
-  delay(1000);
+
+  digitalWrite(sensorPower9, LOW); // turns off power of sensor to increase its life time
+  delay(SLEEP_TIME);
 }
 
 int moisture(int sensorValue) {
-  return (1023 - sensorValue) / 10;
+  return (int)(100.0f * (float)sensorValue / (float)MAX_SENSOR_VALUE);
 }
 
 String moistureToString(int sensorValue) {
-  String label = "Wilgotnosc gleby: ";
+  String label = "Soil moisture: ";
   int valueInPercent = moisture(sensorValue);
   String percentChar = "%";
   String labelWithValue = label + valueInPercent + percentChar;
 
   return labelWithValue;
 }
-
